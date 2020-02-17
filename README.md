d833b61222c7328d95a0176110df50ff4d2dc5eb
Mika Majakorpi <mika.majakorpi@nitor.com>
2020-02-17 13:56:55 +0200

    picocli version

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-Fix the code and run:
-* `mvn clean package`
-* `mvn exec:java -Dexec.mainClass="JwtAssertionGenerator"`
+# JWT Assertion Token Generator
+
+Mainly for this Salesforce OAuth2 flow: https://help.salesforce.com/articleView?id=remoteaccess_oauth_jwt_flow.htm&type=0
+
+Build: `mvn clean package`
+Run: 
+```
+java -jar target/jwt_assert-1.0-SNAPSHOT.jar \
+--audience you_audience \
+--issuer your_issuer \
+--subject your_subject \
+--key /path/to/your/pkcs8_private_key
+```
+
+To create a signing key:
+
+```
+openssl genrsa -des3 -passout pass:SomePassword -out server.pass.key 2048
+openssl rsa -passin pass:SomePassword -in server.pass.key -out server.key
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+openssl pkcs8 -topk8 -inform PEM -outform DER -in server.key -out private_key.pkcs8 -nocrypt
+```
+
+## GraalVM Native Build
+
+Requires a GraalVM JDK and to set `JAVA_HOME=/path/to/a/graalvm/jdk`.
+
+Build with: `mvn clean package -P build-native`
+
+Executable will be written to: `target/net.majakorpi.jwt.JwtAssertionGenerator`
+
+(Works on my machine)
---
+++ src/main/java/net/majakorpi/jwt/JwtAssertionGenerator.java
@@ -0,0 +1,104 @@
+package net.majakorpi.jwt;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+public class JwtAssertionGenerator implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-i", "--issuer"}, description = "Token issuer", required = true)
+    private String issuer = null;
+
+    @CommandLine.Option(names = {"-a", "--audience"}, description = "Token audience", required = true)
+    private String audience = null;
+
+    @CommandLine.Option(names = {"-s", "--subject"}, description = "Token subject", required = true)
+    private String subject = null;
+
+    @CommandLine.Option(names = {"-v", "--validity"}, description = "Token validity in seconds (default 3600)")
+    private Integer validitySeconds = 3600;
+
+    @CommandLine.Option(names = {"-k", "--key"}, description = "Path to PKCS8 private signing key", required = true)
+    private File keyFile = null;
+
+    @CommandLine.Option(names = {"--verbose"}, description = "Output token payload without base64 encoding")
+    private Boolean verbose = false;
+
+    private Base64.Encoder encoder = Base64.getUrlEncoder();
+
+    private static final String UTF_8 = "UTF-8";
+    private static final String DOT = ".";
+    private static final String HEADER = "{\"alg\":\"RS256\"}";
+    private static final String BODY_TEMPLATE =
+            "{\"iss\": \"%s\", \"sub\": \"%s\", \"aud\": \"%s\", \"exp\": %s, \"jti\": \"%s\"}";
+
+    public static void main(String... args) throws Exception {
+        int exitCode = new CommandLine(new JwtAssertionGenerator()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        StringBuilder token = new StringBuilder();
+
+        try {
+            // Prepare claims
+            Long exp = System.currentTimeMillis() / 1000 + validitySeconds;
+            String jti = UUID.randomUUID().toString();
+            String payload = String.format(BODY_TEMPLATE, issuer, subject, audience, exp, jti);
+
+            if (verbose) {
+                System.out.println(payload);
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonElement je = JsonParser.parseString(payload);
+                String prettyJsonString = gson.toJson(je);
+                System.out.println(String.format("Token payload:\n%s\n", prettyJsonString));
+            }
+
+            // Build token
+            token.append(encoder.encodeToString(HEADER.getBytes(UTF_8)))
+                    .append(DOT)
+                    .append(encoder.encodeToString(payload.getBytes(UTF_8)));
+            String signedPayload = signPayload(token.toString());
+            token.append(DOT)
+                    .append(encoder.encodeToString(signedPayload.getBytes(UTF_8)));
+
+            System.out.println(token.toString());
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    private String signPayload(String payload) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException,
+            InvalidKeyException, SignatureException {
+        PrivateKey privateKey = getPrivateKey(keyFile);
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(payload.getBytes(UTF_8));
+        String signedPayload = encoder.encodeToString(signature.sign());
+        return signedPayload;
+    }
+
+    private PrivateKey getPrivateKey(File keyFile) throws IOException, NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        byte[] keyBytes = Files.readAllBytes(keyFile.toPath());
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = kf.generatePrivate(spec);
+        return privateKey;
+    }
+}
